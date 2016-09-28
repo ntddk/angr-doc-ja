f9a805c0f6156e67fb9205bb0be0d535132ddf08
ntddk <ntinternal@gmail.com>
2016-09-28 12:18:30 +0900

    update

---
+++ _book/publish.sh
@@ -0,0 +1,9 @@
+cp -r _book/.git ./.git_tmp
+gitbook build
+cp -r ./.git_tmp _book/.git
+cd _book/
+git checkout gh-pages
+git add --all .
+git commit -m 'update'
+git remote add origin git@github.com:ntddk/angr-doc-ja.git
+git push origin -u gh-pages
